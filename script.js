6f7fb29f1cd315fa97bd4b28460f72b0efec14d9
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-07-13 00:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function calculateMinCost() {
   //your code here
+	let value = document.getElementById('rope-lengths').value;
+  value = value.split(',');
+  let arr = [];
+  for(let i=0;i<value.length;i++){
+    arr[i] = parseInt(value[i]);
+  }
+  arr = arr.sort(function (a,b){ return a-b;});
+  let temp = 0;
+  let result = 0;
+  for(let i=0;i<arr.length;i++){
+    temp = arr[0]+arr[1];
+    result += temp;
+    arr.splice(0,2,temp);
+    arr=arr.sort(function (a,b){ return a-b;});
+  }
+  result += arr[0]+arr[1];
+  
+  let resultFinal = document.getElementById('result');
+  resultFinal.innerText = result;
   
   
   
